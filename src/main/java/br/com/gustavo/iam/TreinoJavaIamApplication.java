e11dd5fd2105d2c3d86965d9137fc22f78bd6ce5
Gustavo Marinho Prado <gmarinhoprado@gmail.com>
2026-06-29 09:32:15 -0300

    feat: migra projeto para Spring Boot

---
+++ src/main/java/br/com/gustavo/iam/TreinoJavaIamApplication.java
@@ -0,0 +1,12 @@
+package br.com.gustavo.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoJavaIamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreinoJavaIamApplication.class, args);
+    }
+}
